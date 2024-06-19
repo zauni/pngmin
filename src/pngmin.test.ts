27e0fae6197e0668cae76fde41a299a6091df8dd
Matthias Zaunseder <zaunseder@outlook.com>
2024-06-19 12:13:24 +0200

    debug unit test

--- src/pngmin.test.ts
+++ src/pngmin.test.ts
@@ -155,6 +155,7 @@ describe("pngmin", () => {
     });
 
     const images = await readdir("tmp/multiple");
+    console.log(log);
 
     assert.ok(images.length === 10, "10 images should be optimized.");
     assert.match(
